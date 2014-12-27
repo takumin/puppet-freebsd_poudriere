9801021b0cf853a09fa99bcbd3fd32837b5454aa
takumin <takumiiinn@gmail.com>
2014-12-27 23:33:02 +0900

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,15 +4,15 @@
 #
 class freebsd_poudriere::config {
 
-  file { $freebsd_poudriere::config:
+  file { $::freebsd_poudriere::config:
     ensure  => file,
     owner   => 'root',
     group   => 'wheel',
     mode    => 0444,
-    content => template($freebsd_poudriere::config_template),
+    content => template($::freebsd_poudriere::config_template),
   }
 
-  file { "$freebsd_poudriere::prefix/etc/poudriere.d":
+  file { "$::freebsd_poudriere::prefix/etc/poudriere.d":
     ensure  => directory,
     owner   => 'root',
     group   => 'wheel',
@@ -22,72 +22,72 @@ class freebsd_poudriere::config {
   #
   # pkgng bulk create
   #
-  file { "$freebsd_poudriere::prefix/etc/poudriere.d/bulk":
+  file { "$::freebsd_poudriere::prefix/etc/poudriere.d/bulk":
     ensure  => directory,
     owner   => 'root',
     group   => 'wheel',
     mode    => 0755,
-    require => File["$freebsd_poudriere::prefix/etc/poudriere.d"],
+    require => File["$::freebsd_poudriere::prefix/etc/poudriere.d"],
   }
 
   #
   # rsync ssh key create
   #
-  file { "$freebsd_poudriere::prefix/etc/poudriere.d/ssh":
+  file { "$::freebsd_poudriere::prefix/etc/poudriere.d/ssh":
     ensure  => directory,
     owner   => 'root',
     group   => 'wheel',
     mode    => 0700,
-    require => File["$freebsd_poudriere::prefix/etc/poudriere.d"],
+    require => File["$::freebsd_poudriere::prefix/etc/poudriere.d"],
   }
   exec { 'poudriere_ssh':
     command => "ssh-keygen -t rsa -P \"\" -C \"poudriere@$::domain\" -f ssh/id_rsa",
     creates => [
-      "$freebsd_poudriere::prefix/etc/poudriere.d/ssh/id_rsa",
-      "$freebsd_poudriere::prefix/etc/poudriere.d/ssh/id_rsa.pub",
+      "$::freebsd_poudriere::prefix/etc/poudriere.d/ssh/id_rsa",
+      "$::freebsd_poudriere::prefix/etc/poudriere.d/ssh/id_rsa.pub",
     ],
-    cwd     => "$freebsd_poudriere::prefix/etc/poudriere.d",
+    cwd     => "$::freebsd_poudriere::prefix/etc/poudriere.d",
     path    => [ '/usr/local/bin', '/usr/bin' ],
-    require => File["$freebsd_poudriere::prefix/etc/poudriere.d/ssh"],
+    require => File["$::freebsd_poudriere::prefix/etc/poudriere.d/ssh"],
   }
 
   #
   # pkgng key file create
   #
-  file { "$freebsd_poudriere::prefix/etc/poudriere.d/keys":
+  file { "$::freebsd_poudriere::prefix/etc/poudriere.d/keys":
     ensure  => directory,
     owner   => 'root',
     group   => 'wheel',
     mode    => 0700,
-    require => File["$freebsd_poudriere::prefix/etc/poudriere.d"],
+    require => File["$::freebsd_poudriere::prefix/etc/poudriere.d"],
   }
   exec { 'poudriere_keys':
     command => 'openssl genrsa -out keys/pkg.key 4096',
-    creates => "$freebsd_poudriere::prefix/etc/poudriere.d/keys/pkg.key",
-    cwd     => "$freebsd_poudriere::prefix/etc/poudriere.d",
+    creates => "$::freebsd_poudriere::prefix/etc/poudriere.d/keys/pkg.key",
+    cwd     => "$::freebsd_poudriere::prefix/etc/poudriere.d",
     path    => [ '/usr/local/bin', '/usr/bin' ],
     require => [
-      File["$freebsd_poudriere::prefix/etc/poudriere.d/keys"],
+      File["$::freebsd_poudriere::prefix/etc/poudriere.d/keys"],
     ],
   }
 
   #
   # pkgng cert file create
   #
-  file { "$freebsd_poudriere::prefix/etc/poudriere.d/certs":
+  file { "$::freebsd_poudriere::prefix/etc/poudriere.d/certs":
     ensure  => directory,
     owner   => 'root',
     group   => 'wheel',
     mode    => 0755,
-    require => File["$freebsd_poudriere::prefix/etc/poudriere.d"],
+    require => File["$::freebsd_poudriere::prefix/etc/poudriere.d"],
   }
   exec { 'poudriere_certs':
     command => 'openssl rsa -in keys/pkg.key -pubout > certs/pkg.crt',
-    creates => "$freebsd_poudriere::prefix/etc/poudriere.d/certs/pkg.crt",
-    cwd     => "$freebsd_poudriere::prefix/etc/poudriere.d",
+    creates => "$::freebsd_poudriere::prefix/etc/poudriere.d/certs/pkg.crt",
+    cwd     => "$::freebsd_poudriere::prefix/etc/poudriere.d",
     path    => [ '/usr/local/bin', '/usr/bin' ],
     require => [
-      File["$freebsd_poudriere::prefix/etc/poudriere.d/certs"],
+      File["$::freebsd_poudriere::prefix/etc/poudriere.d/certs"],
       Exec['poudriere_keys'],
     ],
   }
@@ -95,11 +95,11 @@ class freebsd_poudriere::config {
   #
   # ports tree create
   #
-  create_resources(freebsd_poudriere::ports, $freebsd_poudriere::ports)
+  create_resources(freebsd_poudriere::ports, $::freebsd_poudriere::ports)
 
   #
   # jails create
   #
-  create_resources(freebsd_poudriere::jails, $freebsd_poudriere::jails)
+  create_resources(freebsd_poudriere::jails, $::freebsd_poudriere::jails)
 
 }
