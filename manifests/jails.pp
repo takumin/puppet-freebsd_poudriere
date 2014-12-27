7eeefcdc456795eadac69b5e0e522c81bacf2f0f
takumin <takumiiinn@gmail.com>
2014-12-27 23:22:26 +0900

    fix

--- manifests/jails.pp
+++ manifests/jails.pp
@@ -3,30 +3,65 @@
 # This define is called from freebsd_poudriere for jail create.
 #
 define freebsd_poudriere::jails (
-  $ensure = present,
-  $version,
-  $architecture,
-  $method,
-  $filesystem,
-  $mountpoint,
-  $portstree,
-  $patch,
-  $update,
-  $set,
+  $ensure       = present,
+  $version      = undef,
+  $architecture = undef,
+  $method       = undef,
+  $filesystem   = undef,
+  $mountpoint   = undef,
+  $portstree    = undef,
+  $patch        = undef,
+  $update       = undef,
+  $set          = undef,
 ) {
   if $ensure == present {
+    if $version {
+      $_version = "-v ${version}"
+    }
+    if $architecture {
+      $_architecture = "-a ${architecture}"
+    }
+    if $method {
+      $_method = "-m ${method} "
+    }
+    if $filesystem {
+      $_filesystem = "-f ${filesystem}"
+    }
+    if $mountpoint {
+      $_mountpoint = "-M ${mountpoint}"
+    }
+    if $portstree {
+      $_portstree = "-p ${portstree}"
+
+      Exec["poudriere jail: ${name}"] {
+        require => [
+          freebsd_poudriere::ports[$portstree],
+        ],
+      }
+    }
+    if $patch {
+      $_patch = "-P ${patch}"
+    }
+    if $update {
+      $_update = "-t ${update}"
+    }
+    if $set {
+      $_set = "-z ${set}"
+    }
+
+    $args = "${_version} ${_architecture} ${_method} ${_filesystem} ${_mountpoint} ${_portstree} ${_patch} ${_update} ${_set}"
+
     exec { "poudriere jail: ${name}":
-      command => "poudriere jail -c -j ${name} -v ${version} -a ${architecture} -m ${method} -f ${filesystem} -M ${mountpoint} -p ${portstree} -P ${patch} -t ${update} -z ${set}",
+      command => "poudriere jail -c -j ${name} ${args}",
       path    => [
         '/usr/local/bin',
         '/usr/bin',
       ],
       require => [
         File[$freebsd_poudriere::config],
-        freebsd_poudriere::ports[$portstree],
       ],
       timeout => 3600,
-      unless  => "poudriere jail -qln | grep -qw '^${name}$'",
+      unless  => "poudriere jail -qln | grep -qw '^${name}'",
     }
   }
 }
